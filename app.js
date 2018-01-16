dd3084142e3b70dc2517edb6f4c11375c7da0021
michjadams <Mjadams66@gmail.com>
2018-01-16 13:00:59 -0500

    almost there

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
 //change
 const express = require( 'express' );
 const nunjucks = require('nunjucks');
-const app = express(); // creates an instance of an express application
+const app = express();// creates an instance of an express application
+const routes = require("./routes");
 //const morgan = require('morgan')
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
-// nunjucks.render('index.html', locals, function (err, output) {
-//     console.log(output);
-// });
 
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
 
-app.use(function(req,res,next){
+app.use("/",routes)
 
-	console.log("here "+req.method+" "+ req.path)
-	next()
 
-})
-app.get('/',function(req,res){
-  // res.render('index', locals, function(err, html){
-  //   res.send(html);
-  // })
-  res.render( 'index', locals);
-	//res.send("hello there")
-})
-app.get('/news',function(req,res){
-	res.send("this is the news page")
-})
 
 var listener = app.listen(3000,function(){
 	console.log("listening to port "+ listener.address().port)
